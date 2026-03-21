357bea99fc04e3708f1dc7fd186266b1c5c75ebd
Sarath <sarath@flogic.in>
2026-03-21 16:47:35 -0400

    REDIRECTION ERRO FIX

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ const authRoutes = require('./routes/authRoutes');
 const questionRoutes = require('./routes/questionRoutes');
 const methodOverride = require('method-override');
 
-connectDB();
 const app = express();
 
+connectDB();
 
+app.set('trust proxy', 1);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// Session
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -31,21 +30,24 @@ app.use(
       mongoUrl: process.env.MONGO_URI
     }),
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'lax'
     }
   })
 );
 
-
-
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/layout');
 
-app.use('/', authRoutes);    
-app.use('/questions', questionRoutes); 
+app.use('/', authRoutes);
+app.use('/questions', questionRoutes);
+
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 8080;   
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
